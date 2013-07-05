54c5320f44bfca5bc2131eddf7b8bdbbf13eb145
alaxsxaq <alaxsxaq@gmail.com>
2013-07-05 12:33:18 -0400

    Adding hipchat test files

---
+++ hipchat_glass_bot.js
@@ -0,0 +1,32 @@
+var wobot = require('wobot');
+
+var room_jid = "28742_glenn_-_experimental@conf.hipchat.com"
+var room_nick = "Glass Bot"
+
+var bot = new wobot.Bot({
+    jid: "28742_383441@chat.hipchat.com",
+    password: "fen65hui"
+    });
+    
+bot.connect();
+
+bot.onConnect(function() {
+    console.log('Connected');
+    this.join(room_jid);
+    
+    this.getRoster(function(err, items, stanza) {
+        if (err) {
+            console.log('Error getting roster: ' + err);
+            return;
+        }
+        
+        items.forEach(function(item) {
+            console.log('Roster Contact: ' + item.name);
+        });
+    });
+});
+
+bot.onMessage(function(channel, from, message) {
+    bot.message(channel,message);
+    console.log('> ' + from + '@' + channel + 'said: ' + message);
+});
